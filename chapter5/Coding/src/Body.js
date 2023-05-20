8fa6b3f310cbbd4af0b92e789f98f4bcc8d454f6
yamini-nagireddy <64577525+yamini-nagireddy@users.noreply.github.com>
2023-05-20 20:07:54 +0530

    Update Body.js

--- chapter5/Coding/src/Body.js
+++ chapter5/Coding/src/Body.js
@@ -4,9 +4,31 @@ import { useState } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [searchVal, setSearchVal] = useState("");
 
   return (
     <div className="body-conatiner">
+      <div className="search">
+        <input
+          className="search-ip"
+          type="text"
+          placeholder="Enter Restaurant name..."
+          value={searchVal}
+          onChange={(e) => setSearchVal(e.target.value)}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const newData = listOfRestaurants.filter((resObj) =>
+              resObj.data.name.toLowerCase().includes(searchVal.toLowerCase())
+            );
+            setListOfRestaurants(newData);
+          }}
+          disabled={searchVal == ""}
+        >
+          Search
+        </button>
+      </div>
       <div className="filter">
         <button
           className="filter-btn"
